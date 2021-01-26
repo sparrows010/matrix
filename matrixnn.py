181919a8e27ba94412c1143295b03320e8ce9712
sparrows010 <51452937+sparrows010@users.noreply.github.com>
2021-01-26 18:38:52 +0400

    Create matrixnn.py

---
+++ matrixnn.py
@@ -0,0 +1,18 @@
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+
+Y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+
+result = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+for a in range(len(X)):
+   for b in range(len(X[0])):
+       result[a][b] = X[a][b] + Y[a][b]
+
+for i in result:
+  print(b)
